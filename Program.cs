5e46e07972c0b0ff5d632c2c7157313f55b655ce
Julii Abadjiev <117214264+Jul1ius@users.noreply.github.com>
2023-09-22 10:54:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Transactions;
+
+namespace _1._Number_Pyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+            int current = 1;
+            bool isBigger = false;
+
+            for (int rows = 1; rows <= n; rows++)
+            {
+                for (int cols = 1; cols <= rows; cols++)
+                {
+                    if (current > n)
+                    {
+                        isBigger = true;
+                        break;
+                    }
+                    Console.Write(current + " ");
+                    current++;
+                }
+                if (isBigger)
+                {
+                    break;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
